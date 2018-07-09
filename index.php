ec041b50a6f33e20bf3c2adb506e65579eda0d62
Andre Enriquez <enriquez.evander.andre@gmail.com>
2018-07-09 18:28:09 +0800

    lol

---
+++ index.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Evander Andre Enriquez | Portfolio</title>
+	<!-- META TAGS -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image" href="assets/img/icon.png">
+	<meta name="author" content="Tanislav Robert">
+	<meta name="description" content="KARMO is a creative and modern template for digital agencies">
+
+	<!-- STYLES -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/flexslider.css">
+	<link rel="stylesheet" href="assets/css/animsition.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.css">
+
+	<!-- Fontawesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body class="animsition">
+	<!-- Border -->
+<!-- 	<span class="frame-line top-frame visible-lg"></span>
+	<span class="frame-line right-frame visible-lg"></span>
+	<span class="frame-line bottom-frame visible-lg"></span>
+	<span class="frame-line left-frame visible-lg"></span> -->
+
+				<!-- box-intro -->
+				<section class="box-intro bg-film">
+						<div class="table-cell">
+								<h1>ANDRE <span>ENRIQUEZ</span></h1>
+								<h3 class="box-headline letters rotate-2">
+										<span class="box-words-wrapper">
+												<b class="is-visible">WEB &nbsp;&nbsp; DEVELOPER.</b>
+												<b>&nbsp;PHP &nbsp;&nbsp; DEVELOPER.</b>
+												<b>&nbsp;JAVA &nbsp;&nbsp; DEVELOPER.</b>
+										</span>
+								</h3>
+						</div>
+
+						<div class="mouse">
+								<div class="scroll"></div>
+						</div>
+				</section>
+				<!-- end box-intro -->
+		</div>
+	</header>
+
+	<!-- ABOUT ME -->
+	<section id="about" class="about mt-150 mb-50">
+		<div class="container">
+			<div class="aboutandre">
+					<div class="aboutpic">
+						<img src="assets/img/aboutme.png" width="300"/>
+					</div>
+					<div class="km-space"></div>
+					<h5 class="lead">I am a Web Developer from Paranaque City and a fresh graduate of Polytechnic University of the Philippines. I have serious passion for web development, building excellent web applications that improve the lives of those around me. I specialize in creating websites for clients ranging from individuals and small-businesses.</h5>
+			</div>
+		<!-- description text -->
+		</div>
+				<div class="km-space"></div>
+				<div class="km-space"></div>
+				<div class="sign">
+					<img src="assets/img/signature.png">
+				</div>
+		</div>
+	</section>
+
+	<!-- Services -->
+	<section id="pricing" class="pricing mbr-box mbr-section mbr-section--relative mbr-section--full-height mbr-section--bg-adapted">
+		<div class="section-overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 col-sm-12">
+					<div class="row center">
+						<div class="section-title mb-100">
+							<div class="process-numbers">MY</div>
+							<h2>SERVICES</h2>
+							<p class="module-subtitle">I take great care to ensure each project is well-documented and easily maintainable so you can enhance a website as your company grows.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row center">
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="pricing_plan">
+						<div class="plan_title"><h6>Frontend Development</h6></div>
+						<div class="plan_price">
+							<i class="fa fa-file-code-o" aria-hidden="true"></i>
+                <!-- <div class="numscroller" data-slno='1' data-min='0' data-max='29' data-delay='20' data-increment="5">29</div> -->
+            </div>
+						<ul class="list">
+							<br>
+							<li>I can help you make a website using <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong> as the foundation of my projects.</li>
+							<br>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="pricing_plan" style="background-color:#33bbff;">
+						<div class="plan_title"><h6>Backend Development</h6></div>
+						<div class="plan_price">
+							<i class="fa fa-database" aria-hidden="true"></i>
+                <!-- <div class="numscroller" data-slno='1' data-min='0' data-max='79' data-delay='15' data-increment="5">79</div> -->
+            </div>
+						<ul class="list">
+							<br>
+							<li>Lead Development of web applications using <strong>PHP</strong>, <strong>Java</strong> and <strong>MySQL</strong> database.</li>
+							<!-- <li><strong>Two</strong>year support & updates</li> -->
+							<br>
+							<br>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="pricing_plan">
+						<div class="plan_title"><h6>Website Maintenance</h6></div>
+						<div class="plan_price">
+							<i class="fa fa-cogs" aria-hidden="true"></i>
+            </div>
+						<ul class="list">
+							<br>
+							<li>Performing all the tasks necessary to keep a website up to date and in good, working order.</li>
+							<br>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- PORTFOLIO -->
+	<section class="portfolio">
+
+		<div class="container">
+			<div class="col-md-8 col-md-offset-2 col-sm-12">
+				<div class="row center">
+					<div class="section-title mb-100">
+						<div class="process-numbers">MY</div>
+						<h2>WORKS</h2>
+						<p class="module-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+				</div>
+			</div>
+
+		<div class="myworks">
+
+			<div class="work-item">
+				<a class="portfolio_item work-grid">
+				<img src="assets/img/works/zcasems.png">	
+				<div class="portfolio_item_hover">
+					<div class="item_info">
+						<em>Zinah Christian Academy Student Enrollment & Management System</em><br>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<div class="work-item">
+				<a class="portfolio_item work-grid">
+				<img src="assets/img/works/pup.png">	
+				<div class="portfolio_item_hover">
+					<div class="item_infoo">
+						<em>PUP Faculty Online Syllabus Generator System</em><br>
+					</div>
+					</div>
+				</a>
+			</div>
+
+		</div>
+		</div>
+		<!-- end container -->
+	</section>
+	<!-- portfolio -->
+
+	<!-- CLIENTS -->
+	<div id="clients">
+		<div class="container">
+			<div class="row">
+						<div class="col-md-12">
+						</div>
+						<div id="owl-demo">
+								<div class="item"><img src="assets/img/gnp.png" alt="gnp" width="130px"></div>
+								<div class="item"><img src="assets/img/ibm.png" alt="ibm" width="130px"></div>
+								<div class="item"><img src="assets/img/pup.png" alt="PUP" width="130px"></div>
+								<div class="item"><img src="assets/img/gnp.png" alt="gnp" width="130px"></div>
+								<div class="item"><img src="assets/img/ibm.png" alt="ibm" width="130px"></div>
+								<div class="item"><img src="assets/img/pup.png" alt="PUP" width="130px"></div>
+						</div>
+				</div>
+		</div>
+	</div>
+
+
+
+		<!-- HERO SECTION  -->
+
+	<section class="contact_brick">
+		<div class="container mt-100">
+			<div class="row center">
+				<div class="col-md-8 col-md-offset-2 col-sm-12">
+					<div class="avatar"><img src="assets/img/andre.png"></div>
+					<div class="section-title">
+						<h2 class="mb-50">GET IN TOUCH</h2>
+						<p class="module-subtitle">Wanna get in touch or talk about a project?
+Feel free to contact me via email at <strong>enriquez.evander.andre@gmail.com</strong><br>
+
+or drop a line in the form at the 
+contact section.</p>
+					</div>
+				</div>
+			</div>
+            <div class="row mt-100">
+                    <!--location-->
+                    <div class="col-md-4 col-sm-4 text-center wow slideInLeft">
+                        <div class="detail">
+                            <i class="lnr lnr-map-marker"></i>
+                           <!--  <h4><span>You can find us at</span></h4> -->
+                            <h3>Location</h3>
+                                <p>
+                                    Paranaque City <br>
+                                    Metro Manila, PH
+                                </p>
+                        </div>
+                    </div>
+                    <!--end location-->
+
+                    <!--contact info-->
+                    <div class="col-md-4 col-sm-4 text-center wow slideInUp">
+                        <div class="detail">
+                            <i class="lnr lnr-phone"></i>
+                            <!-- <h4><span>Our mail and phone</span></h4> -->
+                            <h3>Contacts</h3>
+                            <p>
+                                <abbr title="Email">E:</abbr> enriquez.evander.andre@gmail.com
+                                <br>
+                                <abbr title="Phone">P:</abbr> Not yet available.
+                            </p>
+                        </div>
+                    </div>
+                    <!--end contact info-->
+
+                    <!--contact info-->
+                    <div class="col-md-4 col-sm-4 text-center wow slideInRight">
+                        <div class="detail">
+                            <i class="lnr lnr-clock"></i>
+                            <h3>Schedule</h3>
+                            <p>
+                                Mon - Fri 8am - 11pm<br>
+                                Sat - Sun 9am - 12pm 
+                            </p>
+                        </div>
+                    </div>
+                    <!--end contact info-->
+                </div>
+            <!-- CONTACT FORM -->
+            <div class="contact-form bottom">
+                <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        <input type="text" name="name" id="name" class="form-control" required="required" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="company" id="company" class="form-control" required="required" placeholder="Company">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" id="email" class="form-control" required="required" placeholder="Email Id">
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button id="submit_btn" class="default-btn gray-btn"> Send <i class="fa fa-angle-double-right" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+                </form>
+            </div>
+		</div><!-- end container -->
+	</section>
+	<!-- Footer -->
+	<footer class="main-footer">
+            <div class="container">
+                <div class="row mt-100 mb-50 footer-widgets">
+                    <!-- Start Contact Widget -->
+                    <div class="footer-c">
+                        <div class="footer-widget contact-widget">
+                            <ul class="social-icons">
+                                <li>
+                                    <a class="facebook" href="https://www.facebook.com/xNayeon" target="_blank"><i class="fa fa-facebook"></i></a>
+                                </li>
+
+                                <li>
+                                    <a class="linkdin" href="https://www.linkedin.com/in/andreenriquez/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                           
+                                <li>
+                                    <a class="instgram" href="https://www.instagram.com/evander.andre/" target="_blank"><i class="fa fa-instagram"></i></a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div><!-- .col-md-3 -->
+                    <!-- End Contact Widget -->
+
+          
+                </div><!-- .row -->
+
+                <!-- Start Copyright -->
+                <div class="copyright-section">
+                    <div class="row">
+                        <div class="footer-c">
+                            <p>&copy; 2018.  All Rights Reserved <a>Andre Enriquez</a> </p>
+                        </div><!-- .col-md-6 -->
+                    </div><!-- .row -->
+                </div>
+                <!-- End Copyright -->
+            </div>
+	</footer>
+
+	<!-- SCRIPTS -->
+	<script type="text/javascript" src="assets/js/jquery-2.2.3.min.js"></script>
+	<script type="text/javascript" src="assets/js/animated-headline.js"></script>
+	<script type="text/javascript" src="assets/js/menu.js"></script>
+	<script type="text/javascript" src="assets/js/modernizr.js"></script>
+	<script type="text/javascript" src="assets/js/isotope.pkgd.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.flexslider-min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.animsition.min.js"></script>
+	<script type="text/javascript" src="assets/js/init.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+	<script type="text/javascript" src="assets/js/smooth-scroll.js"></script>
+	<script type="text/javascript" src="assets/js/numscroller.js"></script>
+	<script type="text/javascript" src="assets/js/wow.min.js"></script>
+	<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+
+	<script type="text/javascript">
+		$(window).load(function() {
+			new WOW().init();
+
+			// initialise flexslider
+			$('.flexslider').flexslider({
+				animation: "fade",
+				directionNav: true,
+				controlNav: false,
+				keyboardNav: true,
+				slideToStart: 0,
+				animationLoop: true,
+				pauseOnHover: false,
+				slideshowSpeed: 4000,
+			});
+
+			smoothScroll.init();
+
+			// initialize isotope
+			var $container = $('.portfolio_container');
+			$container.isotope({
+				filter: '*',
+			});
+
+			$('.portfolio_filter a').click(function(){
+				$('.portfolio_filter .active').removeClass('active');
+				$(this).addClass('active');
+
+				var selector = $(this).attr('data-filter');
+				$container.isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 500,
+						animationEngine : "jquery"
+					}
+				});
+				return false;
+			});
+		});
+	</script>
+</body>
+</html>
